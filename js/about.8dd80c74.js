ad383b9a39b300e907cf585c1e71cc25b05dbcee
陳建佑 <jianyou224@gmail.com>
2024-12-14 16:53:11 +0800

    deploy

---
+++ js/about.8dd80c74.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunk_2023chart_test"]=self["webpackChunk_2023chart_test"]||[]).push([[443],{9801:function(t,n,s){s.r(n),s.d(n,{default:function(){return l}});var u=function(){var t=this;t._self._c;return t._m(0)},e=[function(){var t=this,n=t._self._c;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")])])}],a=s(3736),r={},c=(0,a.Z)(r,u,e,!1,null,null,null),l=c.exports}}]);
+//# sourceMappingURL=about.8dd80c74.js.map
